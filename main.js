352c656c70099aa5c1e1efdba5373c425ba79218
thfirmin <thfirmin.dsantos@gmail.com>
2023-08-25 16:00:15 -0300

    test

---
+++ main.js
@@ -0,0 +1,5 @@
+import {hello, hello2} from "./module.js"
+//const hello = require('./module')
+
+hello();
+hello2();
---
+++ module.js
@@ -0,0 +1,12 @@
+export function hello() {
+	console.log("Hello, World!");
+}
+
+export const hello2 = () => {
+	console.log("HELLO 2");
+}
+
+//module.exports = {hello}
+//com uma função use o 'default'
+//tenha um package.json com "type":"module"
+//use npm init ou yarn init 
