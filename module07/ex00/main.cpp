4c56c3dbffdcf0f32f261152b1068179eb3de19f
Thfirmin <thfirmin.dsantos+github@gmail.com>
2023-11-13 19:49:48 -0300

    Module07: update ex00

--- module07/ex00/main.cpp
+++ module07/ex00/main.cpp
@@ -6,13 +6,33 @@
 /*   By: thfirmin <thfirmin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 19:08:07 by thfirmin          #+#    #+#             */
-/*   Updated: 2023/11/13 19:45:07 by thfirmin         ###   ########.fr       */
+/*   Updated: 2023/11/13 19:47:24 by thfirmin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "whatever.hpp"
 
+int	main( void )
+{
+	int	a = 2;
+	int	b = 3;
+
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+
+	return 0;
+}
+
+/*
 int	main(void)
 {
 	double	elem1 = 3.5;
@@ -28,3 +48,4 @@ int	main(void)
 	std::cout << "elem2 [" << elem2 << "]" << std::endl;
 	return (0);
 }
+*/
