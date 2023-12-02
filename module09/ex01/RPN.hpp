3b2e35ee626c532b8f42d7f905e8741e4a0e1c2e
thfirmin <thfirmin.dsantos@gmail.com>
2023-12-02 18:27:45 -0300

    Module09: ex01 updated

--- module09/ex01/RPN.hpp
+++ module09/ex01/RPN.hpp
@@ -6,20 +6,28 @@
 /*   By: thfirmin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/02 17:57:38 by thfirmin          #+#    #+#             */
-/*   Updated: 2023/12/02 18:01:45 by thfirmin         ###   ########.fr       */
+/*   Updated: 2023/12/02 18:27:29 by thfirmin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef RPN_HPP
 # define RPN_HPP
 
+#include <stack>
+
 class	RPN {
 	public:
+		void start(std::string expr);
+
+	private:
 		RPN(void);
 		RPN(const RPN& src);
 		~RPN(void);
 
+		std::stack<char>	_stack;
+
 		RPN&	operator=(const RPN& src);
+
 };
 
 #endif
